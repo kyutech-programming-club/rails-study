dd4412fdaea5c1d03415a1d3ae273eaf85675546
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-06-21 19:00:34 +0900

    rails g model Post content:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_110017) do
+ActiveRecord::Schema.define(version: 2019_06_21_095256) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
